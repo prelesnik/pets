3f20932114541e2081b51e92318ef49afaf63d45
Brandon Skar <bransk01@yahoo.com>
2019-01-11 13:28:24 -0500

    Created composer, ran fat free, created index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Brandon
+ * Date: 1/11/2019
+ * Time: 10:24 AM
+ */
+
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require fat-free
+require_once('vendor/autoload.php');
+
+//Create an instance of the Base class
+$f3 = Base::instance();
+
+//Turn of fat free error reporting
+$f3->set('DEBUG', 3);
+
+//Define a default route
+$f3->route('GET /', function() {
+    echo "Pet Home";
+    /*$view = new View;
+    echo $view->render();*/
+});
+
+//Run fat-free
+$f3->run();
