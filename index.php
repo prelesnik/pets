56f1eddbab13e193872c79cd8a696602c94493ff
Mike Prelesnik <mprelesnik@mail.greenriver.edu>
2019-01-11 13:37:54 -0500

    added a homepage and modified index.php to render the view

--- index.php
+++ index.php
@@ -21,9 +21,9 @@ $f3->set('DEBUG', 3);
 
 //Define a default route
 $f3->route('GET /', function() {
-    echo "Pet Home";
-    /*$view = new View;
-    echo $view->render();*/
+    //echo "Pet Home";
+    $view = new View;
+    echo $view->render('views/home.html');
 });
 
 //Run fat-free
